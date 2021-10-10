01d3d1be4496b34d74ec949b1fa3955c90cab159
Marcus Berget <marcusberget@gmail.com>
2021-10-10 13:51:03 +0200

    endringer av plottekode

--- Project1/main.py
+++ Project1/main.py
@@ -1,21 +1,15 @@
 import numpy as np
 from regression_methods import OLSReg, RidgeReg, LassoReg
 from common import FrankeFunction
-from plot import (MSE_R2, bias_variance, error_of_polydeg, owncode_vs_sklearn,
-error_of_lambda)
+from plot import (MSE_R2, bias_variance, error_of_polydeg, owncode_vs_sklearn, test_vs_train,
+error_of_lambda, variance_of_lambda, bias_variance_of_lambda, bias_variance_poly)
 
-<<<<<<< HEAD
 N = 20
-poly_degrees = np.arange(1, 10)
-N_boostraps = 1
-=======
-N = 50
 poly_degrees = np.arange(1, 15)
-N_boostraps = 100
->>>>>>> f5ea6cc4ea8663c64faaf8d63046e23464467b0f
+N_boostraps = 20
 k = 5
-N_lambdas = 9
-lambdas = np.logspace(-4, 1, N_lambdas)
+N_lambdas = 20
+lambdas = np.logspace(-4, 2, N_lambdas)
 
 np.random.seed(2018)
 
@@ -34,68 +28,89 @@ def produce_results_OLS():
     model = OLSReg(poly_degrees)
     error, r2, bias, variance = model.simple_regression(x, y, z, 0, True)
     MSE_R2(
-        poly_degrees, error, r2,
-        title='OLS regression with simple resampling',
-        fname=f'plots/exercise1_MSE_R2score{N}.pdf'
+        poly_degrees, error, r2, N,
+        title=f'OLS, MSE vs R2, {N}x{N}',
+        fname=f'plots/OLS/OLS_MSE_R2score{N}.pdf'
     )
-    bias_variance(
-        poly_degrees, error, bias, variance,
-        title='OLS regression with simple resampling',
-        fname=f'plots/exercise2_bias_variance{N}.pdf'
+    test_vs_train(
+        poly_degrees, error, N,
+        title=f'OLS regression, no resampling, {N}x{N} datapoints',
+        fname=f'plots/OLS/OLS_Test_vs_train_{N}.pdf'
     )
 
     error, bias, variance = model.bootstrap(x, y, z, [0], N_boostraps)
     bias_variance(
-        poly_degrees, error, bias, variance,
+        poly_degrees, error, bias, variance, N,
         title='OLS regression with bootstrapping',
-        fname=f'plots/exercise2_bootstrap{N}.pdf'
+        fname=f'plots/OLS/OLS_BiasVariance_BS_{N}.pdf'
     )
 
     error, error_sklearn = model.k_fold(xy, x, y, z, [0], k)
     error_of_polydeg(
         poly_degrees, error,
         title='OLS regression with k-foldning',
-        fname=f'plots/exercise3_k-fold{N}.pdf'
+        fname=f'plots/OLS/OLS_kfold_MSE{N}.pdf'
     )
 
     owncode_vs_sklearn(poly_degrees, error[:, :, 0], error_sklearn[:, 0],
-        title='OLS regression with k-foldning',
-        fname=f'plots/exercise3_k-fold_sklearn{N}.pdf'
+        title='OLS, k-foldning. Own code VS sklearn',
+        fname=f'plots/OLS/OLS_k-fold_sklearn{N}.pdf'
     )
 
 """ RIDGE; SIMPLE, BOOTSTRAP, K-FOLD """
 def produce_results_ridge():
+
     model = RidgeReg(poly_degrees)
+    '''
     lmb = 0.5
     error, _, bias, variance = model.simple_regression(x, y, z, lmb, True)
     bias_variance(
         poly_degrees, error, bias, variance,
         title=r'Ridge regression with simple resampling for $\lambda = $' + f'{lmb}',
         fname=f'plots/exercise4_bias_variance_lmb_{lmb}_{N}.pdf'
-    )
-    error, bias, variance = model.bootstrap(x, y, z, lambdas, N_boostraps)
-    for i in range(0, N_lambdas, 2):
-        bias_variance(
-            poly_degrees, error[:, :, i], bias[:, :, i], variance[:, :, i],
-            title=r'Ridge regression with bootstrapping for $\lambda = $' + f'{lambdas[i]:.2e}',
-            fname=f'plots/exercise4-bootstrap_{i+1}_{N}.pdf'
         )
+    '''
+
+    error, bias, variance = model.bootstrap(x, y, z, lambdas, N_boostraps)
+    mean_bias = np.zeros_like(bias[:, :, 0])
+    mean_variance = np.zeros_like(variance[:, :, 0])
+    for i in range(0, N_lambdas):
+        mean_bias += bias[:, :, i]
+        mean_variance += variance[:, :, i]
+    mean_bias = mean_bias/N_lambdas
+    mean_variance = mean_variance/N_lambdas
+    bias_variance_poly(
+        poly_degrees, mean_bias, bias[:, :, i], mean_variance,
+        title=r'Bias Variance trend, taken as mean over all $\lambda$, )' + f'{N}x{N}',
+        fname=f'plots/Ridge/Ridge_BiasVariance_BS_mean_{N}.pdf'
+    )
+    deg_idx = 1
+    lmb_idx = 1
+    bias_variance_of_lambda(
+        lambdas, variance[:, deg_idx], bias[:, deg_idx],
+        title=f'Ridge bias-variance, {N}x{N}, p={poly_degrees[deg_idx]}',
+        fname=f'plots/Ridge/Ridge_BiasVariance_BS_lambda_{poly_degrees[deg_idx]:.2e}_{N}.pdf'
+    )
+
     error, error_sklearn = model.k_fold(xy, x, y, z, lambdas, k)
-    deg_idx = 4
-    lmb_idx = 3
+    variance_of_lambda(
+        lambdas, variance[:, deg_idx],
+        title=f'Ridge regression with k-foldning for polynomial degree {poly_degrees[deg_idx]}',
+        fname=f'plots/Ridge/kfold/Ridge_variance_k-fold_lambda_deg_{poly_degrees[deg_idx]:.2e}_{N}.pdf'
+    )
     error_of_polydeg(
         poly_degrees, error[:, :, lmb_idx],
         title=r'Ridge regression with k-foldning for $\lambda = $' + f'{lambdas[lmb_idx]:.2e}',
-        fname=f'plots/exercise4_k-fold_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf'
+        fname=f'plots/Ridge/kfold/Ridge_k-fold_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf'
     )
     owncode_vs_sklearn(poly_degrees, error[:, :, lmb_idx], error_sklearn[:, lmb_idx],
         title='Ridge regression with k-foldning for $\lambda = $' + f'{lambdas[lmb_idx]:.2e}',
-        fname=f'plots/exercise4_k-fold_sklearn_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf')
+        fname=f'plots/Ridge/kfold/Ridge_k-fold_sklearn_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf')
 
     error_of_lambda(
         lambdas, error[:, deg_idx],
         title=f'Ridge regression with k-foldning for polynomial degree {poly_degrees[deg_idx]}',
-        fname=f'plots/exercise4_k-fold_lambda_deg_{poly_degrees[deg_idx]:.2e}_{N}.pdf'
+        fname=f'plots/Ridge/kfold/Ridge_k-fold_lambda_deg_{poly_degrees[deg_idx]:.2e}_{N}.pdf'
     )
 
 """ LASSO; SIMPLE, BOOTSTRAP, K-FOLD """
@@ -104,43 +119,38 @@ def produce_results_lasso():
     model = LassoReg(poly_degrees)
     error, _, bias, variance = model.simple_regression(x, y, z, lmb, True)
     bias_variance(
-        poly_degrees, error, bias, variance,
+        poly_degrees, error, bias, variance, N,
         title=r'Lasso regression with simple resampling for $\lambda = $' + f'{lmb}',
-        fname=f'plots/exercise5_bias_variance_lmb_{lmb}_{N}.pdf'
+        fname=f'plots/Lasso/Lasso_bias_variance_lmb_{lmb}_{N}.pdf'
     )
 
     error, bias, variance = model.bootstrap(x, y, z, lambdas, N_boostraps)
     for i in range(0, N_lambdas, 2):
         bias_variance(
-            poly_degrees, error[:, :, i], bias[:, :, i], variance[:, :, i],
+            poly_degrees, error[:, :, i], bias[:, :, i], variance[:, :, i], N,
             title=r'Lasso regression with bootstrapping for $\lambda = $' + f'{lambdas[i]:.2e}',
-            fname=f'plots/exercise5-bootstrap_{i+1}_{N}.pdf'
+            fname=f'plots/Lasso/Lasso_BS_{i+1}_{N}.pdf'
         )
     error, error_sklearn = model.k_fold(xy, x, y, z, lambdas, k)
-    deg_idx = 4
-    lmb_idx = 3
+    deg_idx = 1
+    lmb_idx = 1
     error_of_polydeg(
         poly_degrees, error[:, :, lmb_idx],
         title=r'Lasso regression with k-foldning for $\lambda = $' + f'{lambdas[lmb_idx]:.2e}',
-        fname=f'plots/exercise5_k-fold_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf'
+        fname=f'plots/Lasso/Lasso_k-fold_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf'
     )
 
     owncode_vs_sklearn(poly_degrees, error[:, :, lmb_idx], error_sklearn[:, lmb_idx],
         title='Lasso regression with k-foldning for $\lambda = $' + f'{lambdas[lmb_idx]:.2e}',
-        fname=f'plots/exercise5_k-fold_sklearn_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf'
+        fname=f'plots/Lasso/Lasso_k-fold_sklearn_lmb_{lambdas[lmb_idx]:.2e}_{N}.pdf'
     )
 
     error_of_lambda(
         lambdas, error[:, deg_idx],
         title=f'Lasso regression with k-foldning for polynomial degree {poly_degrees[deg_idx]}',
-        fname=f'plots/exercise5_k-fold_lambda_deg_{poly_degrees[deg_idx]:.2e}_{N}.pdf'
+        fname=f'plots/Lasso/Lasso_k-fold_lambda_deg_{poly_degrees[deg_idx]:.2e}_{N}.pdf'
     )
 
-produce_results_OLS()
-<<<<<<< HEAD
-#produce_results_ridge()
-#produce_results_lasso()
-=======
-# produce_results_ridge()
-# produce_results_lasso()
->>>>>>> f5ea6cc4ea8663c64faaf8d63046e23464467b0f
+#produce_results_OLS()
+produce_results_ridge()
+produce_results_lasso()
--- Project1/plot.py
+++ Project1/plot.py
@@ -5,11 +5,11 @@ plt.style.use('seaborn')
 plt.rc('text', usetex=True)
 plt.rc('text.latex', preamble=r'\usepackage{amsmath}')
 
-def MSE_R2(poly_degrees, MSE, R2, title, fname):
+def MSE_R2(poly_degrees, MSE, R2, N, title, fname):
     fig, axes = plt.subplots(1, 2)
     ax = axes[0]
-    fig.suptitle(title, fontsize=20)
-    ax.set_title('Mean Square Error', fontsize=15)
+    #fig.suptitle(title, fontsize=20)
+    ax.set_title(f'Mean Square Error, {N}x{N}', fontsize=15)
     ax.plot(poly_degrees, MSE[0], label="Training data")
     ax.plot(poly_degrees, MSE[1], label="Test data")
     ax.set_xlabel('Polynomial Degree', fontsize=15)
@@ -17,7 +17,7 @@ def MSE_R2(poly_degrees, MSE, R2, title, fname):
     ax.legend(fontsize=15)
 
     ax = axes[1]
-    ax.set_title('R2 score', fontsize=15)
+    ax.set_title(f'R2 score, {N}x{N}', fontsize=15)
     ax.plot(poly_degrees, R2[0], label="Training data")
     ax.plot(poly_degrees, R2[1], label="Test data")
     ax.set_xlabel('Polynomial Degree', fontsize=15)
@@ -26,22 +26,47 @@ def MSE_R2(poly_degrees, MSE, R2, title, fname):
     fig.savefig(fname)
     plt.close(fig)
 
-def bias_variance(poly_degrees, MSE, bias, variance, title, fname):
+def test_vs_train(poly_degrees, MSE, N, title, fname):
+    poly_degrees_new = np.arange(1, len(poly_degrees), 2)
+    fig, ax = plt.subplots()
+    ax.set_title(title, fontsize=20)
+    ax.set_xticks(poly_degrees_new)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    ax.set_title(f'Training vs test data, MSE, {N}x{N} datapoints', fontsize=18)
+    ax.plot(poly_degrees, MSE[0], label='MSE, Training')
+    ax.plot(poly_degrees, MSE[1], label='MSE, Test')
+    ax.set_xlabel('Polynomial Degree', fontsize=18)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    ax.legend(fontsize=18)
+    fig.savefig(fname)
+    plt.close(fig)
+
+def bias_variance(poly_degrees, MSE, bias, variance, N, title, fname):
+    poly_degrees_new = np.arange(1, len(poly_degrees), 2)
+    fig, ax = plt.subplots()
+    ax.set_title(title, fontsize=20)
+    ax.set_xticks(poly_degrees_new)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    ax.set_title(f'Test data, {N}x{N} datapoints', fontsize=18)
+    ax.plot(poly_degrees, MSE[1], label='MSE')
+    ax.plot(poly_degrees, bias[1], label='Bias')
+    ax.plot(poly_degrees, variance[1], label='Variance')
+    ax.set_xlabel('Polynomial Degree', fontsize=18)
+    ax.tick_params(axis='both', which='major', labelsize=18)
+    ax.legend(fontsize=18)
+    fig.savefig(fname)
+    plt.close(fig)
+
+def bias_variance_poly(poly_degrees, MSE, bias, variance, title, fname):
     fig, axes = plt.subplots(1, 2)
     fig.suptitle(title, fontsize=20)
     ax = axes[0]
-    ax.set_title('Bias-variance trade-off for training data', fontsize=15)
-    ax.plot(poly_degrees, MSE[0], label='MSE')
-    ax.plot(poly_degrees, bias[0], label='Bias')
-    ax.plot(poly_degrees, variance[0], label='Variance')
+    ax.plot(poly_degrees, variance[1], label='Variance')
     ax.set_xlabel('Polynomial Degree', fontsize=15)
     ax.legend(fontsize=15)
 
     ax = axes[1]
-    ax.set_title('Bias-variance trade-off for test data', fontsize=15)
-    ax.plot(poly_degrees, MSE[1], label='MSE')
     ax.plot(poly_degrees, bias[1], label='Bias')
-    ax.plot(poly_degrees, variance[1], label='Variance')
     ax.set_xlabel('Polynomial Degree', fontsize=15)
     ax.legend(fontsize=15)
     fig.savefig(fname)
@@ -88,3 +113,64 @@ def plot_terrain(terrain):
     ax.set_ylabel('Y', fontsize=15)
     fig.savefig('plot/exercise6_terrain.pdf')
     plt.close(fig)
+
+def variance_of_lambda(lambdas, var_lmbd, title, fname):
+    fig, ax = plt.subplots()
+    ax.set_title(title, fontsize=20)
+    ax.plot(np.log10(lambdas), var_lmbd[0], label = 'variance, train data')
+    ax.plot(np.log10(lambdas), var_lmbd[1], label = 'variance, test data')
+    ax.set_xlabel(r'$\log_{10}(\lambda)$', fontsize=15)
+    ax.set_ylabel('variance', fontsize=15)
+    ax.legend(fontsize=15)
+    fig.savefig(fname)
+    plt.close(fig)
+
+def bias_variance_of_lambda(lambdas, var, bias, title, fname):
+    var_min = np.argmin(var[1])
+    fig, ax = plt.subplots(1,2)
+    fig.suptitle(title, fontsize=20)
+    ax[0].set_title('variance')
+    ax[0].plot(np.log10(lambdas), var[1], label = 'variance')
+    ax[0].scatter(np.log10(lambdas[var_min]), var[1][var_min], label = r'$\lambda$ =' + f'{lambdas[var_min]:.2f}')
+    ax[1].set_title('Bias')
+    ax[1].plot(np.log10(lambdas), bias[1], label = 'Bias')
+    ax[1].scatter(np.log10(lambdas[var_min]), bias[1][var_min], label = r'$\lambda$ =' + f'{lambdas[var_min]:.2f}')
+    ax[0].set_xlabel(r'$\log_{10}(\lambda)$', fontsize=15)
+    ax[1].set_xlabel(r'$\log_{10}(\lambda)$', fontsize=15)
+    ax[0].legend(fontsize=15)
+    ax[1].legend(fontsize=15)
+    fig.savefig(fname)
+    plt.close(fig)
+
+def model_terrain(X, x, y, beta, degree, N):
+    xx, yy = np.meshgrid(x,y)
+    z_model = X@beta
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.set_title(f'p={degree}')
+    surf = ax.plot_surface(xx, yy, z_model, cmap=cm.coolwarm,
+    linewidth=0, antialiased=False)
+    ax.set_zlim(-0.10, 1.40)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.savefig(f'surf_plot_p{degree}_N{N}.png')
+
+def CI(beta):
+    diag_ele = np.zeros(len(beta))
+    X_i = np.linalg.pinv(X.T@X)
+    for i in range(len(beta)):
+        diag_ele[i] = X_i[i,i]
+    var_beta = var_error*diag_ele
+    betas = np.arange(1, len(beta) + 1)
+    CI_u = beta + 1.96*var_beta/np.sqrt(N)
+    CI_l = beta - 1.96*var_beta/np.sqrt(N)
+    yerr = 2*var_beta/np.sqrt(N)
+    plt.fill_between(betas, CI_u, CI_l, alpha = 0.9)
+    plt.scatter(betas, beta, marker = 'x', color = 'red', label = 'beta')
+    plt.title(r'$\beta$ values with corresponding confidence interval of 95$\%$,' + f'p={degree}, n={N}')
+    plt.xlabel(r'$\beta$')
+    plt.xticks(betas)
+    plt.ylabel(r'$\beta$ values')
+    plt.legend()
+    plt.savefig(f'conf_plot_p{degree}_N{N}.png')
