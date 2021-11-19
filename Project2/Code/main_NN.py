13b4ab58b7c4782beb60f970290d7b92cb812b53
Marcus Berget <marcusberget@gmail.com>
2021-11-19 14:05:22 +0100

    lagt til en print-funskjon som stopper når toleranse er nådd

--- Project2/Code/main_NN.py
+++ Project2/Code/main_NN.py
@@ -32,17 +32,17 @@ X[:,1] = y
 #Produce target data
 z = np.ravel(FrankeFunction(xx, yy))
 z = np.reshape(z, (len(z), 1))
-n_layers_list = [1,2,3]#[1,2,3,4,5,6]
-n_hidden_neurons_list = [5,10,20]
+n_layers_list = [2,3,4]
+n_hidden_neurons_list = [5]
 n_inputs = X.shape[0]
 n_features = X.shape[1]
 n_outputs = n_inputs
 
 epochs = 3000
 eta = 5e-4
-lmbd_list = [1e-4] #[1e1, 1e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6]
+lmbd_list = [1e-4]
 gamma = 0.9
-tol = 0.013
+tol = 0.014
 batch_size_list = [500]
 t0, t1 = [5e-4], 100 # Paramters used in learning rate.
 
@@ -59,82 +59,92 @@ for lmbd in lmbd_list:
                     z_h, a_h, z_o_sigmoid, a_L = sigmoid_model.feed_forward(X_test)
                     MSE_sigmoid = np.mean((z_test - z_o_sigmoid)**2)
                     f.write(f'Sigmoid   |  {(time_sigmoid-start):.1f} | {MSE_sigmoid:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-                    #plot_surface(X, sigmoid_model, 'Sigmoid', epochs, n_layers, xx, yy, N)
-# plot_surface(X, TANH_model, 'tanh', epochs, n_layers, xx, yy, N)
+                    plot_surface(X, sigmoid_model, 'Sigmoid', epochs, n_layers, n_hidden_neurons, xx, yy, N)
 
-t0, t1 = [5e-4], 100 # Paramters used in learning rate.
-lmbd_list = [1e-4]
-for lmbd in lmbd_list:
-    for batch_size in batch_size_list:
-        for n_layers in n_layers_list:
-            for n_hidden_neurons in n_hidden_neurons_list:
-                for eta in t0:
-                    start = time.time()
-                    TANH_model = Tang_hyp(eta, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
-                    TANH_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
-                    z_h, a_h, z_o_tanh, a_L = TANH_model.feed_forward(X_test)
-                    time_tanh = time.time()
-                    MSE_tanh = np.mean((z_test - z_o_tanh)**2)
-                    f.write(f'Tanh      | {(time_tanh-start):.1f} | {MSE_tanh:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-                    #plot_surface(X, TANH_model, 'tanh', epochs, n_layers, xx, yy, N)
+# n_layers_list = [4] #,2,3,4]#,5,6]
+# n_hidden_neurons_list = [5] #, 10, 20]
+# t0, t1 = [5e-4], 100 # Paramters used in learning rate.
+# lmbd_list = [1e-4]
+# for lmbd in lmbd_list:
+#     for batch_size in batch_size_list:
+#         for n_layers in n_layers_list:
+#             for n_hidden_neurons in n_hidden_neurons_list:
+#                 for eta in t0:
+#                     start = time.time()
+#                     TANH_model = Tang_hyp(eta, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
+#                     TANH_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
+#                     z_h, a_h, z_o_tanh, a_L = TANH_model.feed_forward(X_test)
+#                     time_tanh = time.time()
+#                     MSE_tanh = np.mean((z_test - z_o_tanh)**2)
+#                     f.write(f'Tanh      | {(time_tanh-start):.1f} | {MSE_tanh:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
+#                     plot_surface(X, TANH_model, 'tanh', epochs, n_layers, n_hidden_neurons, xx, yy, N)
 
-
-t0, t1 = [1e-4], 100 # Paramters used in learning rate.
-lmbd_list = [5e-4]
-for lmbd in lmbd_list:
-    for batch_size in batch_size_list:
-        for n_layers in n_layers_list:
-            for n_hidden_neurons in n_hidden_neurons_list:
-                for eta in t0:
-                    start = time.time()
-                    RELU_model = RELU(eta, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
-                    RELU_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
-                    time_relu = time.time()
-                    z_h, a_h, z_o_relu, a_L = RELU_model.feed_forward(X_test)
-                    time_relu = time.time()
-                    MSE_relu = np.mean((z_test - z_o_relu)**2)
-                    f.write(f'Relu      | {(time_relu-start):.1f} | {MSE_relu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-                    #plot_surface(X, RELU_model, 'RELU', epochs, n_layers, xx, yy, N)
-f.close()
-quit()
-
-
-
-eta = 0.001 #Learning rate
-RELU_model = RELU(t0, t1, lmbd, gamma, n_layers, n_hidden_neurons, X_train, 'regression')
-RELU_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
-time_relu = time.time()
-z_h, a_h, z_o_relu, a_L = RELU_model.feed_forward(X_test)
-time_relu = time.time()
-MSE_relu = np.mean((z_test - z_o_relu)**2)
-f.write(f'Relu      | {(time_relu-start):.1f} | {MSE_relu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-
-ELU_model = ELU(t0, t1, lmbd, gamma, n_layers, n_hidden_neurons, X_train, 'regression')
-ELU_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
-z_h, a_h, z_o_elu, a_L = ELU_model.feed_forward(X_test)
-time_elu = time.time()
-MSE_elu = np.mean((z_test - z_o_elu)**2)
-f.write(f'Elu       | {(time_elu-start):.1f} | {MSE_relu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-
-HS_model = Heaviside(t0, t1, lmbd, gamma, n_layers, n_hidden_neurons, X_train, 'regression')
-HS_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
-z_h, a_h, z_o_HS, a_L = HS_model.feed_forward(X_test)
-time_HS = time.time()
-MSE_HS = np.mean((z_test - z_o_HS)**2)
-f.write(f'Heaviside | {(time_HS-start):.1f} | {MSE_relu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-
-Leaky_model = Leaky(t0, t1, lmbd, gamma, n_layers, n_hidden_neurons, X_train, 'regression')
-Leaky_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
-z_h, a_h, z_o_leaky, a_L = Leaky_model.feed_forward(X_test)
-time_leaky = time.time()
-MSE_leaky = np.mean((z_test - z_o_leaky)**2)
-f.write(f'Leaky     | {(time_leaky-start):.1f} | {MSE_relu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
-
-f.close()
-
-plot_surface(X, sigmoid_model, 'Sigmoid', epochs, n_layers, xx, yy, N)
-plot_surface(X, TANH_model, 'tanh', epochs, n_layers, xx, yy, N)
-plot_surface(X, RELU_model, 'RELU', epochs, n_layers, xx, yy, N)
-plot_surface(X, ELU_model, 'ELU', epochs, n_layers, xx, yy, N)
-plot_surface(X, Leaky_model, 'Leaky', epochs, n_layers, xx, yy, N)
-plot_surface(X, HS_model, 'Heaviside', epochs, n_layers, xx, yy, N)
+# t0, t1 = [1e-4], 100 # Paramters used in learning rate.
+# lmbd_list = [5e-4]
+# n_layers_list = [6,7,8,9,10] #,2,3,4]#,5,6]
+# n_hidden_neurons_list = [15] #, 10, 20]
+# for lmbd in lmbd_list:
+#     for batch_size in batch_size_list:
+#         for n_layers in n_layers_list:
+#             for n_hidden_neurons in n_hidden_neurons_list:
+#                 for eta in t0:
+#                     start = time.time()
+#                     RELU_model = RELU(eta, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
+#                     RELU_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
+#                     time_relu = time.time()
+#                     z_h, a_h, z_o_relu, a_L = RELU_model.feed_forward(X_test)
+#                     time_relu = time.time()
+#                     MSE_relu = np.mean((z_test - z_o_relu)**2)
+#                     f.write(f'Relu      | {(time_relu-start):.1f} | {MSE_relu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
+#                     plot_surface(X, RELU_model, 'RELU', epochs, n_layers, n_hidden_neurons, xx, yy, N)
+# quit()
+# t0, t1 = [1e-5], 100 # Paramters used in learning rate.
+# lmbd_list = [5e-4]
+# for lmbd in lmbd_list:
+#     for batch_size in batch_size_list:
+#         for n_layers in n_layers_list:
+#             for n_hidden_neurons in n_hidden_neurons_list:
+#                 for eta in t0:
+#                     start = time.time()
+#                     Leaky_model = Leaky(t0, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
+#                     Leaky_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
+#                     z_h, a_h, z_o_leaky, a_L = Leaky_model.feed_forward(X_test)
+#                     time_leaky = time.time()
+#                     MSE_leaky = np.mean((z_test - z_o_leaky)**2)
+#                     f.write(f'Leaky     | {(time_leaky-start):.1f} | {MSE_leaky:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
+#                     plot_surface(X, Leaky_model, 'Leaky', epochs, n_layers, xx, yy, N)
+#
+# t0, t1 = [1e-5], 100 # Paramters used in learning rate.
+# lmbd_list = [5e-4]
+# for lmbd in lmbd_list:
+#     for batch_size in batch_size_list:
+#         for n_layers in n_layers_list:
+#             for n_hidden_neurons in n_hidden_neurons_list:
+#                 for eta in t0:
+#                     start = time.time()
+#                     HS_model = Heaviside(t0, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
+#                     HS_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
+#                     z_h, a_h, z_o_HS, a_L = HS_model.feed_forward(X_test)
+#                     time_HS = time.time()
+#                     MSE_HS = np.mean((z_test - z_o_HS)**2)
+#                     f.write(f'Heaviside | {(time_HS-start):.1f} | {MSE_HS:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
+#                     plot_surface(X, HS_model, 'Heaviside', epochs, n_layers, xx, yy, N)
+#
+# t0, t1 = [1e-5], 100 # Paramters used in learning rate.
+# lmbd_list = [5e-5]
+# for lmbd in lmbd_list:
+#     for batch_size in batch_size_list:
+#         for n_layers in n_layers_list:
+#             for n_hidden_neurons in n_hidden_neurons_list:
+#                 for eta in t0:
+#                     start = time.time()
+#                     ELU_model = ELU(t0, t1, lmbd, gamma, tol, n_layers, n_hidden_neurons, X_train, 'regression')
+#                     ELU_model.train(X_train, z_train, epochs, batch_size, learning_schedule=lambda t,t0,t1: t0)
+#                     z_h, a_h, z_o_elu, a_L = ELU_model.feed_forward(X_test)
+#                     time_elu = time.time()
+#                     MSE_elu = np.mean((z_test - z_o_elu)**2)
+#                     f.write(f'Elu       | {(time_elu-start):.1f} | {MSE_elu:.3f}|  {lmbd}  |     {eta}     |   {n_layers}    |        {n_hidden_neurons}         |   {epochs}   | \n')
+#                     plot_surface(X, ELU_model, 'ELU', epochs, n_layers, xx, yy, N)
+#
+# f.close()
+# quit()
--- Project2/Code/methods_NN.py
+++ Project2/Code/methods_NN.py
@@ -38,6 +38,15 @@ class NeuralNetwork:
             self.output_bias = np.zeros((1 , 1)) + 0.01
 
         elif self.init_method() == "Xavier":
+            lim1 = np.sqrt(1/(n_inputs))
+            self.input_weights = np.random.uniform(low = -lim1, high = lim1, size = (n_features, n_hidden_neurons))
+            lim2 = np.sqrt(1/(n_hidden_neurons))
+            self.hidden_weights = np.random.uniform(low = -lim2, high = lim2, size = (n_layers - 1, n_hidden_neurons, n_hidden_neurons))
+            self.hidden_bias = np.zeros((n_layers, n_hidden_neurons)) #+ 0.01
+            self.output_weights = np.random.uniform(low = -lim1, high = lim1, size = (n_hidden_neurons, 1))
+            self.output_bias = np.zeros((1 , 1)) #+ 0.01
+
+        elif self.init_method() == "Xavier_norm":
             lim1 = np.sqrt(1/(n_inputs+n_hidden_neurons))
             self.input_weights = np.random.uniform(low = -lim1, high = lim1, size = (n_features, n_hidden_neurons))
             lim2 = np.sqrt(6/(2*n_hidden_neurons))
@@ -132,20 +141,22 @@ class NeuralNetwork:
         N = int(X.shape[0])
         rng = np.random.default_rng(1234)
         indices = np.arange(N)
-        for epoch in range(epochs):
+        for epoch in range(epochs+1):
             rng.shuffle(indices)
             X_s = X[indices]
             z_s = z[indices]
             if self.MSE < self.tol:
                 print(f'Tolerance of {self.tol} reached at epoch={epoch}   | {self} | lmbd= {self.lmbd} | eta= {eta} | layers = {self.n_layers} | neurons = {self.n_hidden_neurons} |')
                 break
+            elif epoch == epochs:
+                print(f'Tolerance not reached, stopped training at epoch={epoch}')
             for i in range(0, N, batch_size):
                 eta = learning_schedule(epoch*(N/batch_size)+i, self.t0, self.t1)
                 self.back_propagation(X_s[i:i+batch_size], z_s[i:i+batch_size], eta)
 
 class Sigmoid(NeuralNetwork):
     def init_method(self):
-        return "Xavier"
+        return "He"
 
     def __str__(self):
         return 'Sigmoid'
@@ -159,7 +170,7 @@ class Sigmoid(NeuralNetwork):
 
 class Tang_hyp(NeuralNetwork):
     def init_method(self):
-        return "Xavier"
+        return "Random"
 
     def __str__(self):
         return 'tanh'
@@ -190,7 +201,7 @@ class RELU(NeuralNetwork):
 
 class ELU(NeuralNetwork):
     def init_method(self):
-        return "Random"
+        return "He"
 
     def activation_func(self, z):
         a = z.copy()
@@ -205,7 +216,7 @@ class ELU(NeuralNetwork):
 
 class Leaky(NeuralNetwork):
     def init_method(self):
-        return "Random"
+        return "He"
 
     def activation_func(self, z):
         a = z.copy()
